e6dea512ad8280d12f00649054eafda79941be67
Rohit <rohitsahay.199626@gmail.com>
2017-09-17 01:28:13 -0700

    TestApp

---
+++ app/src/main/java/com/example/rohit/testapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.rohit.testapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mSignUp;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSignUp = (TextView) findViewById(R.id.main_signup_textview);
+        mSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,SignupActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
