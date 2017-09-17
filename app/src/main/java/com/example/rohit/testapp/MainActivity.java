fc4fa59ad2c6291c59eaa3052abec535f473bdeb
Rohit <rohitsahay.199626@gmail.com>
2017-09-17 10:08:07 -0700

    Updated17/09

--- app/src/main/java/com/example/rohit/testapp/MainActivity.java
+++ app/src/main/java/com/example/rohit/testapp/MainActivity.java
@@ -6,20 +6,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
     private TextView mSignUp;
+    private TextView mResetPass;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mResetPass = (TextView) findViewById(R.id.main_resetpass_textview);
+
+        mResetPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i0 = new Intent(MainActivity.this,ResetPass.class);
+                startActivity(i0);
+            }
+        });
+
         mSignUp = (TextView) findViewById(R.id.main_signup_textview1);
         mSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this,SignupActivity.class);
-                startActivity(i);
+                Intent i1 = new Intent(MainActivity.this,SignupActivity.class);
+                startActivity(i1);
             }
         });
     }
